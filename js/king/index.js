bee06a4cee072ba0257b20efd266c0a657361ed5
Emensionyu <2833359326@qq.com>
2018-09-18 23:59:17 +0800

    starwar

--- js/king/index.js
+++ js/king/index.js
@@ -2,6 +2,7 @@ const chior=[]; //乐队
 
 // 只有会嘎嘎叫的才要 check（）
 // 代码 流程 组成乐队 办事流程、
+// js是动态类型语言区别于java
 var duck={
     duckSing:function(){
         console.log('嘎嘎嘎');
@@ -9,6 +10,16 @@ var duck={
 
 
 }
+var chicken={
+    chickSing:function(){
+        console.log('咯咯咯');
+    },
+    duckSing:function(){
+        console.log('嘎嘎嘎');
+    }
+}
+// 走起来像鸭子，叫起来也像鸭子，那么他就是鸭子，duck
+//  typing ja弱类型
 
 /*
 
@@ -17,7 +28,8 @@ var duck={
   chior加入合唱团
 */
 // 1000只，999只duck、
-function joinChior(animal) {
+function joinChior
+(animal) {
     if (animal && typeof animal.duckSing=='function') {
         chior.push(animal);
     }else{
@@ -32,13 +44,15 @@ for(var i=0;i<999;i++){
     joinChior(duck);
 
 }
-console.log(chior.length);
-joinChior();
+// 现学 js 类 对象 ，动态的
 
-var chicken={
-    chickSing:function(){
-        console.log('咯咯咯');
-    }
+chicken.duckSing=function(){
+    console.log('嘎嘎嘎');
 }
+console.log(chior.length);
+joinChior(chichen);
+
+// 只有999只鸭子
+
 
 
