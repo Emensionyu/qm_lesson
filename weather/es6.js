9d3fe02ffbca8c3b1ca6b73097553ed240758aca
Emensionyu <2833359326@qq.com>
2018-11-29 19:29:02 +0800

    correct

--- weather/es6.js
+++ weather/es6.js
@@ -1,14 +1,10 @@
-const num=9;
-let isMarried=false//唐僧
+const num = 9;
+let isMarried = false//唐僧
 
 
 // es6=>es5//支持VRA
 // var 
-
-let yourTurn=['好工作','找老板谈','驴妈妈网'].map((task,index)=>{
+let yourTurn = ['好工作', '找老板谈', '驴妈妈网'].map((task, index) => {
     return `${index}-${task}`
 });
-
-
-
 console.log(yourTurn);
