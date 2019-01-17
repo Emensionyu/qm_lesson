ae2eb1c70811d746fba26582364e82d7eef800e3
Emensionyu <2833359326@qq.com>
2019-01-17 19:16:19 +0800

    1.17 promise源码

--- js/promise_souce_code/index.js
+++ js/promise_souce_code/index.js
@@ -1,16 +1,26 @@
 const Promise=require('./promise.js')
-const p=new Promise((resolve,reject)=>{
-    console.log("哈哈哈")
-    setTimeout(()=>{
-        console.log("哈哈哈")
-        resolve('aaa')
-    },2000)
+// const p=new Promise((resolve,reject)=>{
+//     // console.log("哈哈哈")
+//     setTimeout(()=>{
+//         // console.log("哈哈哈")
+//         resolve('aaa')
+//     },2000)
   
-    })
-p
-  .then((data)=>{
-      console.log(data);
-  },err=>{
-      console.log(err);
-  })
+//     })
+
+//     p
+//   .then((data)=>{
+//       console.log(data);
+//   },err=>{
+//       console.log(err);
+//   })
 //   console.log('b');
+
+const b=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('aaa')
+        },2000)
+})
+b.then(data=>{
+    console.log(data);
+},err=>{console.log(err)})
--- js/promise_souce_code/promise2.js
+++ js/promise_souce_code/promise2.js
@@ -13,23 +13,32 @@ class Promise {
       this.reason = undefined;
       this.onRejectedCallBacks=[];
       this.onResolveCallBacks=[];
-      let resolve = (value) => {  
-        this.status = 'resolved'; 
+      let resolve = (value) => { 
+         //then 的时候传递给resolve值同时执行 resolve('aaa')
+         console.log('resolve 开始被初始化')
+        this.status = 'resolved'; //
         this.value = value;//aaa
         this.onResolveCallBacks.forEach(fn=>fn())
+        console.log('resolve已经被使用')
       }
+      // resolve('aaa')
+      // console.log(resolve+"我是resolve");
       let reject = (reason) => {
         this.status = 'rejected';
         this.value = reason;
         this.onRejectedCallBacks.forEach(fn=>fn())
       }
       executor(resolve, reject);
+      // console.log(executor(resolve, reject));
       // 开启了？  。。。
       // new 异步任务开始执行 
     }
     then (onFullfield, onRejected) {
         if(this.status==="pending"){
-            this.onResolveCallBacks.push(()=>{onFullfield(this.value)});
+            this.onResolveCallBacks.push(()=>{
+              console.log('resolve已经放进去了 等待then的执行');
+              onFullfield(this.value)
+            });
             this.onRejectedCallBacks.push(()=>{onRejected(this.value)});
         }
     //   if (this.status === 'resolved') {
